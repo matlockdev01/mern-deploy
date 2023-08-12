8b3da60f270d1f65f8bd09c8c81a3fd31a97bde0
Josh Matlock <matlocktranslation@gmail.com>
2023-08-12 06:25:33 -0500

    connected frontend to backend

--- backend/app.js
+++ backend/app.js
@@ -2,3 +2,26 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const corse = require("cors")
+require("dotenv").config();
+
+//middleware
+const corsOptions = {
+    origin: "https://mern-deploy-frontend-d13m.onrender.com", 
+}
+app.use(express.json());
+app.use(cors(corsOptions))
+
+// connect MongoDB
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    const PORT = process.env.PORT || 8000
+    app.listen(PORT, () => {
+        console.log(`App is listening on PORT ${PORT}`);
+    })
+}).catch(err => {
+    console.log(err);
+});
+
+//route 
+app.get("/", (req, res) => {
+    res.status(201).json({message: "Connected to Backend!"});
+});
